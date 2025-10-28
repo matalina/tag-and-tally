357a5d9b6360bb7a6c322ae9fd7fac18ca7ff395
Alicia Wilkerson <matalina@gmail.com>
2025-10-28 15:07:36 -0500

    publishing default

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -21,7 +17,5 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-header {
-  background-color: var(--surface-4);
-}
+
 </style>
