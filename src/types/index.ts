92e078b2930b01fd835d47885223a2971d86c2c6
Alicia Wilkerson <matalina@gmail.com>
2025-11-27 14:18:45 +0000

    added types

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,17 @@ export interface User {
   picture: string
   sub: string
 }
+
+export interface RandomTable {
+  name: string;
+  formula: string;
+  table: TableOption[];
+}
+
+export interface TableOption {
+  min: number | null;
+  max: number | null;
+  value: string | RandomFunction;
+}
+
+export type RandomFunction =(table: RandomTable) => string;
